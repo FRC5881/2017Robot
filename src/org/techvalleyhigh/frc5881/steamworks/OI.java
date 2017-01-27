fa43f7fef60cee7f77973955dce8aeb0a38f40c2
Kyle <ksharpe@techvalleyhigh.org>
2017-01-27 16:51:20 -0500

    Created OI Class, needs commands which are to be planned then added.

---
+++ src/org/techvalleyhigh/frc5881/steamworks/OI.java
@@ -0,0 +1,45 @@
+package org.techvalleyhigh.frc5881.steamworks;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class OI {
+
+    public GenericHID xboxController;
+    public JoystickButton aButton;
+    public JoystickButton bButton;
+    public JoystickButton xButton;
+    public JoystickButton yButton;
+    public JoystickButton backButton;
+    public JoystickButton startButton;
+    public Joystick ljoystick;
+    public Joystick rjoystick;
+
+    public OI() {
+
+        xboxController = new Joystick(0) {
+        };
+
+        // Button 1 == A Button
+        // Button 2 == B Button
+        // Button 3 == X Button
+        // Button 4 == Y Button
+        // Button 7 == backButton
+        // Button 8 == startButton
+
+        aButton = new JoystickButton(xboxController, 1);
+        bButton = new JoystickButton(xboxController, 2);
+        xButton = new JoystickButton(xboxController, 3);
+        yButton = new JoystickButton(xboxController, 4);
+        backButton = new JoystickButton(xboxController, 7);
+        startButton = new JoystickButton(xboxController, 8);
+
+
+    }
+
+    public GenericHID getXboxController() {return xboxController;}
+
+}
