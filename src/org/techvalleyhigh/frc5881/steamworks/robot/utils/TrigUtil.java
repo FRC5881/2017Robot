b95c6f1b9bc68e0639d641f2da56003ec7ef96ac
Brian Rozmierski <brianr@warped-minds.com>
2017-02-13 14:26:39 -0500

    Added comments, made static functions.

--- src/org/techvalleyhigh/frc5881/steamworks/robot/utils/TrigUtil.java
+++ src/org/techvalleyhigh/frc5881/steamworks/robot/utils/TrigUtil.java
@@ -1,7 +1,5 @@
 package org.techvalleyhigh.frc5881.steamworks.robot.utils;
 
-import java.math.BigDecimal;
-
 /**
  * Created by ksharpe on 2/11/2017.
  */
@@ -10,36 +8,64 @@ public class TrigUtil {
 
     public static double getHypotenuseFromSides(double a, double b) {
 
-        return Math.sqrt(a*a + b*b);
+        return Math.sqrt(a * a + b * b);
     }
 
     public static double getSideFromHypotenuseAndSide(double c, double a) {
 
-        return Math.sqrt(a*a - c*c);
+        return Math.sqrt(a * a - c * c);
     }
 
     public static double getOppositeFromAngleAndHypotenuse(double c, double theta) {
 
         double sinOfTheta = Math.sin(theta);
 
-            return c * sinOfTheta;
-        }
+        return c * sinOfTheta;
+    }
 
-    public double angleGiven;
-    public double distanceGiven;
-    public double deadZone = 40;
-    double theta = 90 - angleGiven;
-    double a = TrigUtil.getOppositeFromAngleAndHypotenuse(distanceGiven, angleGiven);
-    double a1 = a - deadZone;
-    double b = TrigUtil.getSideFromHypotenuseAndSide(distanceGiven, a);
-
-    public double findDistanceToLineUpWithGear(double deadZone, double distanceGiven, double angleGiven) {
-
+    /**
+     * Find the distance to travel to align before a final push into the gear peg.
+     *
+     * @param deadZone Distance to stop short of the peg by, robot should be center to peg at this distance when travel
+     *                 is complete.
+     * @param distanceGiven Current distance from the robot to the center of the vision target.
+     * @param angleGiven Current angle, measured from center of bot as 0 degrees, to the target, positive angle to the
+     *                   right.
+     * @return Distance to travel, in inches, to align to center of the peg.
+     */
+    public static double findDistanceToLineUpWithGear(double deadZone, double distanceGiven, double angleGiven) {
+        /* Given a triangle, formed with a hypotenuse defined as distanceGiven, an external angle, complement of the
+         * right angle of angleGiven, form a triangle with sides a and b. Then reduce the length of the a side by
+         * the deadZone value (to stop short) and recompute the hypotenuse and inside angles.
+         */
+        double theta = 90 - angleGiven;
+        double a = TrigUtil.getOppositeFromAngleAndHypotenuse(distanceGiven, theta);
+        double a1 = a - deadZone;
+        double b = TrigUtil.getSideFromHypotenuseAndSide(distanceGiven, a);
         return TrigUtil.getHypotenuseFromSides(b, a1); //Distance to travel
     }
 
-    public double findAngleToTurnToLineUpWithGear(double deadZone, double distanceGiven, double angleGiven) {
-
+    /**
+     * Find the angle to run to, inclusive of the dead zone to shop short from, to align to the peg.
+     *
+     * @param deadZone Distance to stop short of the peg by, robot should be center to peg at this distance when travel
+     *                 is complete.
+     * @param distanceGiven Current distance from the robot to the center of the vision target.
+     * @param angleGiven Current angle, measured from center of bot as 0 degrees, to the target, positive angle to the
+     *                   right.
+     * @return Angle to turn, measured from center of robot as 0 degrees, with a positive value turning right,
+     *         prior to travel, to align to the peg. At the end of travel the negated value of this angle should
+     *         result in the bot facing directly at the peg.
+     */
+    public static double findAngleToTurnToLineUpWithGear(double deadZone, double distanceGiven, double angleGiven) {
+        /* Given a triangle, formed with a hypotenuse defined as distanceGiven, an external angle, complement of the
+         * right angle of angleGiven, form a triangle with sides a and b. Then reduce the length of the a side by
+         * the deadZone value (to stop short) and recompute the hypotenuse and inside angles.
+         */
+        double theta = 90 - angleGiven;
+        double a = TrigUtil.getOppositeFromAngleAndHypotenuse(distanceGiven, theta);
+        double a1 = a - deadZone;
+        double b = TrigUtil.getSideFromHypotenuseAndSide(distanceGiven, a);
         return Math.atan(a1 / b);
     }
-    }
+}
