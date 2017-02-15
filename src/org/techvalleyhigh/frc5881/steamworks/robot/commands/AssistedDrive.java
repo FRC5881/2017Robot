5f1e257fa97dbbc4d69ff704a079a1905b05e7ac
CMahoney <cmahoney@techvalleyhigh.org>
2017-02-15 11:52:28 -0500

    Cleaned up DriveControl Talons, Added PhysicsUtil class, worked on the shooter class (define motors, stop motor spin, etc), Clean up RobotMap (a lot).

---
+++ src/org/techvalleyhigh/frc5881/steamworks/robot/commands/AssistedDrive.java
@@ -0,0 +1,8 @@
+package org.techvalleyhigh.frc5881.steamworks.robot.commands;
+
+/**
+ * Created by CMahoney on 2/13/2017.
+ */
+public class AssistedDrive extends Command {
+    //TODO assisted driving and turning
+}
