96a057b4dc301e3395b1632fe17530ac37a5d565
CMahoney <cmahoney@techvalleyhigh.org>
2017-10-29 09:06:37 -0400

    Starting work on auto

--- src/org/techvalleyhigh/frc5881/steamworks/robot/commands/AutonomousCommand.java
+++ src/org/techvalleyhigh/frc5881/steamworks/robot/commands/AutonomousCommand.java
@@ -11,88 +11,37 @@ import org.techvalleyhigh.frc5881.steamworks.robot.utils.TrigUtil;
  */
 //TODO: Organize this mess better
 public class AutonomousCommand extends CommandGroup {
+    private String routine;
+    public AutonomousCommand(String Autoroutine) {
+        routine = Autoroutine;
+        run();
+    }
+
+    private void run() {
+        // Default is going to be null because we want to make sure drive team has to select an autonomous run
+        if (routine != "null") {
+            if(routine == "Baseline") {
+                // Crosses Baseline (10 feet just to be safe)
+                addSequential(new AssistedDrive(10, 0));
+
+            } else if (routine == "Left Peg") {
+                //While we don't see the
+            } else if (routine == "Right Peg") {
+
+            } else if (routine == "Testing") {
+                //Test arc pathing
+                addSequential(new ArcPathing(36, 36, 0.65));
+
+            }
+        }
+    }
+
     @Override
     protected boolean isFinished() {
         return false;
     }
 
-    public AutonomousCommand CommandGroup;
+    private void scoreGear(boolean turnLeft) {
 
-    PegUtil pegSnapShot;
-
-    /**
-     * Much of the "Score Gear" autonomous code repeats for each starting position
-     */
-    private void scoreGear() {
-        //Save a snapshot into memory
-        //Find peg
-        //Debug for testing
-        //Calculate distance and degrees to turn (accounting for camera displacement)
-        //Turn Degrees
-        //Drive Distance
-        //Turn -Degrees
-        //Take new snapshot
-        //Recalculate Degrees && Turn
-        //Recalculate Distance && Drive, if the bot is too close it will start getting wrong readings
-    }
-
-    public AutonomousCommand(String Autoroutine) {
-
-        // Default is going to be null because we want to make sure drive team has to select an autonomous run
-        if (Autoroutine != "null") {
-
-            // x' == x" / 12
-
-            if(Autoroutine == "Gear Boiler") {
-                // Move to pass base line move out of the key
-                addSequential(new AssistedDrive(60/12, 0));
-
-                //Score gear
-                scoreGear();
-            } else if(Autoroutine == "Gear Not Boiler") {
-                // Starting anywhere other than the key
-                scoreGear();
-
-            } else if(Autoroutine == "Baseline") {
-                // Crosses Baseline (10 feet just to be safe)
-                addSequential(new AssistedDrive(10, 0));
-
-            } else if (Autoroutine == "pos1-b") {
-                // Backward 20" == 1.6667'
-                addSequential(new AssistedDrive(1.6667, 0));
-
-            } else if (Autoroutine == "pos1-o") {
-                // Backward 50" == 4.1667'
-                addSequential(new AssistedDrive(4.1667, 0));
-                // Turn clockwise 50 degrees
-                addSequential(new AssistedDrive(0, 50));
-
-            } else if (Autoroutine == "pos2-l") {
-                // Backward 20" == 1.6667'
-                addSequential(new AssistedDrive(20, 0));
-                //Turn counter-clockwise 90 degrees
-                addSequential(new AssistedDrive(0, -90));
-
-            } else if (Autoroutine == "pos2-r") {
-                // Backward 20" == 1.6667'
-                addSequential(new AssistedDrive(1.6667, 0));
-                // Turn clockwise 90 degrees
-                addSequential(new AssistedDrive(0, 90));
-
-            } else if (Autoroutine == "pos3-b") {
-                //  50" == 4.1667'
-                addSequential(new AssistedDrive(4.1667, 0));
-
-            } else if (Autoroutine == "pos3-0") {
-                // Backward 50" == 4.1667'
-                addSequential(new AssistedDrive(4.1667, 0));
-                // Turn clockwise 90 degrees
-                addSequential(new AssistedDrive(0, 90));
-
-            } else if (Autoroutine == "Curve") {
-                //Test arc pathing
-                addSequential(new ArcPathing(36, 36, 0.65));
-            }
-        }
     }
 }
